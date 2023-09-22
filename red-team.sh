a067f89c04b2a1c5d4e0df785ab9058ad35bd095
lakmerol <lakmerol@gmail.com>
2023-09-22 21:00:34 +0300

    first page text changed

--- red-team.sh
+++ red-team.sh
@@ -1,9 +1,7 @@
 #!/bin/bash
 
 
-# Check if /tmp/onepage folder exists
 if [ ! -d "/tmp/onepage" ]; then
-  # If it doesn't exist, clone the GitHub repository
   git clone https://github.com/lakmerol/onepage.git /tmp/onepage
 fi
 
@@ -14,5 +12,6 @@ git pull
 sed -i "s/ISIM/$1/g" index.html
 sed -i "s/EPOSTA/$2/g" index.html
 
-cp -r * /var/www/html/
+sudo cp -r * /var/www/html/
 
+echo "Script run automaticly"
